6077cde5c5e961d4183beeefc384761746f39b06
unknown <antu.haks@gmail.com>
2024-07-31 18:03:19 +0600

    Edited Error Page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>Error Page</h1>
+        <div className="bg-gradient-to-r from-green-200 to-green-100 min-h-screen flex items-center justify-center">
+            <div className="w-9/12 m-auto py-16">
+                <div className="bg-white shadow overflow-hidden sm:rounded-lg pb-8">
+                    <div className="border-t border-gray-200 text-center pt-8">
+                        <h1 className="text-9xl font-bold text-green-400">404</h1>
+                        <h1 className="text-6xl font-medium py-8">Oops! Page not found</h1>
+                        <p className="text-2xl pb-8 px-12 font-medium">
+                            Oops! The page you are looking for does not exist. It might have been moved or deleted.
+                        </p>
+                        <Link to={`/`}
+                            className="bg-gradient-to-r from-green-400 to-blue-400 hover:from-pink-500 hover:to-orange-500 text-white font-semibold px-6 py-3 rounded-md mr-6"
+                        >
+                            HOME
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
